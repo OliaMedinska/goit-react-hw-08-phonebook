190c49279d571c8f76a0f26a5c3234c3970f75e7
OliaMedinska <medinska2@gmail.com>
2024-01-03 23:55:40 +0100

    login and register

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import {App} from './components/App';
 import './index.css';
 import { Provider } from 'react-redux';
@@ -7,7 +9,9 @@ import { store} from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
   <Provider store={store}>
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
   </Provider>
   </>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,26 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import contactsReducer from "./contactsSlice";
 import filtersSlice from "./filtersSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth/slice';
 
+// Persisting token field from auth slice to localstorage
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
@@ -9,5 +28,17 @@ const rootReducer = combineReducers({
 })
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    rootReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
